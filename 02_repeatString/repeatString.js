23f0f071491a6892131a7580e46de2843d75eb4f
chenste-osu <chen.st.we@gmail.com>
2023-01-13 14:48:16 -0500

    complete exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    if ((num < 0)) {
+        return "ERROR";
+    }
 
+    let stringArray = [];
+
+    for (let i = 0; i < num; i++) {
+        stringArray.push(string);
+    }
+
+    let newString = stringArray.join("");
+    return newString;
 };
 
 // Do not edit below this line
