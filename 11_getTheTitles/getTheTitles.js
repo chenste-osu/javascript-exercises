a544d149eea121bf24b0cc66ac05f26f4cbe764e
chenste-osu <chen.st.we@gmail.com>
2023-01-17 17:29:38 -0500

    complete exercise

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,11 @@
-const getTheTitles = function() {
+const getTheTitles = function(bookArray) {
+    let titleArray = [];
 
+    for (const book of bookArray) {
+        titleArray.push(book.title);
+    }
+
+    return titleArray;
 };
 
 // Do not edit below this line
