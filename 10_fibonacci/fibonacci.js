22cf1e3079c3e145b580354b36ccc2269dc76f44
chenste-osu <chen.st.we@gmail.com>
2023-01-17 17:19:12 -0500

    complete exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,28 @@
-const fibonacci = function() {
+const fibonacci = function(place) {
+    if (typeof place == 'string') {
+        place = parseInt(place);
+    }
+    if (place < 0) {
+        return 'OOPS';
+    }
 
+    for (let i = 0; i < place + 1; i++) {
+        answer = recur(i);
+    }
+
+    return answer;
+
+    function recur(num) {
+        if (num < 2) {
+            return num;
+        }
+        else {
+            return recur(num - 1) + recur(num - 2);
+        }
+    }
 };
+
+console.log(fibonacci(25));
 
 // Do not edit below this line
 module.exports = fibonacci;
