e50c94fd367b3122ea1d9bc4ede14522e7754465
chenste-osu <chen.st.we@gmail.com>
2023-01-17 18:55:04 -0500

    complete exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,47 @@
-const findTheOldest = function() {
+const findTheOldest = function(peopleArray) {
 
+    const getOldest = function(accum, current) {
+        const accumAge = getAge(accum.yearOfBirth, accum.yearOfDeath);
+        const currentAge = getAge(current.yearOfBirth, current.yearOfDeath);
+
+        if (currentAge > accumAge) {
+            return current;
+        } else {
+            return accum;
+        }
+    }
+
+    return peopleArray.reduce(getOldest);
 };
+
+const getAge = function(birth, death) {
+    if (!death) {
+        death = new Date().getFullYear();
+    }
+    return death - birth;
+};
+
+// const people = [
+//     {
+//       name: "Carly",
+//       yearOfBirth: 1942,
+//       yearOfDeath: 1970,
+//     },
+//     {
+//       name: "Ray",
+//       yearOfBirth: 1962,
+//       yearOfDeath: 2011,
+//     },
+//     {
+//       name: "Jane",
+//       yearOfBirth: 1912,
+//       yearOfDeath: 1941,
+//     },
+//   ];
+
+// console.log(findTheOldest(people));
+
+// findTheOldest(people);
 
 // Do not edit below this line
 module.exports = findTheOldest;
