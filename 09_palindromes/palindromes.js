bdda1f29f4ebcced25339b591227c6f9859e3cee
chenste-osu <chen.st.we@gmail.com>
2023-01-17 16:48:42 -0500

    complete exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,30 @@
-const palindromes = function () {
+const palindromes = function (word) {
+    word = word.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"")
+        .replace(/\s+/g, "");
+    word = word.toLowerCase();
 
+    // console.log(word);
+
+    let left = 0;
+    let right = word.length - 1;
+
+    while (left < right) {
+        // console.log(left);
+        // console.log(word[left]);
+        // console.log(right);
+        // console.log(word[right]);
+
+        if (word[left] != word[right]){
+            return false
+        }
+        left++;
+        right--;
+    }
+
+    return true;
 };
+
+palindromes('Animal loots foliated detail of stool lamina.');
 
 // Do not edit below this line
 module.exports = palindromes;
