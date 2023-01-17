8c111896be70799ce4d076a7388a28bbe47306e6
chenste-osu <chen.st.we@gmail.com>
2023-01-17 16:35:39 -0500

    complete exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,48 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	if (!array) {
+    return 0;
+  }
+
+  let sum = 0;
+
+  for (let i = 0; i < array.length; i++) {
+    sum += array[i]
+  }
+
+  return sum;
 };
 
-const multiply = function() {
+const multiply = function(array) {
+  let total = 1;
 
+  for (num of array) {
+    total *= num;
+  }
+  
+  return total;
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+	return Math.pow(num1, num2);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let total = 1;
+
+	while (num > 0) {
+    total *= num;
+    num -= 1;
+  }
+
+  return total;
 };
 
 // Do not edit below this line
