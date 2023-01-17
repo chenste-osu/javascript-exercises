61d8c514e16ce552d660f9b76412e8f13f9f3e7d
chenste-osu <chen.st.we@gmail.com>
2023-01-17 15:53:15 -0500

    complete exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    if (!(Number.isInteger(num1) && Number.isInteger(num2))) {
+        return 'ERROR';
+    }
 
+    if ((num1 < 0) || (num2 < 0)) {
+        return 'ERROR';
+    }
+
+    let left = Math.min(num1, num2);
+    let right = Math.max(num1, num2);
+    let final = 0;
+
+    for (let i = left; i < right + 1; i++) {
+        final += i;
+    }
+    return final;
 };
 
 // Do not edit below this line
